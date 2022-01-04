5294b4013ea2d72ccc5d3fd6585127aa8171cf6b
izdihar <btmkpt@gmail.com>
2022-01-04 15:36:04 +0800

    Tambah list ul dan li

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ad officiis eius magni consectetur labore omnis animi excepturi modi, est quos. Atque, sunt minus autem eos voluptates ratione mollitia nemo.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
